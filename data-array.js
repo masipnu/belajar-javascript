f2e7864a69851d592216e714856c6168b25cf814
masipnu <masipnu1@gmail.com>
2024-04-14 00:08:05 +0700

    method pada data array

---
+++ data-array.js
@@ -0,0 +1,26 @@
+// array kosong
+let peserta = [];
+
+// array string
+let warna = ["merah", "kuning", "hijau", "biru"];
+
+// array angka
+let angka = [1, 2, 3, 4, 5];
+
+// array campuran
+let sesuatu = [true, 1, "kucing", null];
+
+// beberapa method yang digunakan pada array
+angka.push(1); // menambah isi array dari belakang
+angka.pop(); // mengurangi 1 elemen array dari belakang
+angka.shift(); // menngurangi 1 elemen array dari depan
+angka.unshift(0); // menambah elemen array dari belakang
+
+// concat   : menggabungkan array
+// includes : mencari suatu nilai di dalam array
+// indexOf  : sama kaya string.indexOf
+// join     : membuat suatu string dari sebuah array
+// reverse  : membalikkan urutan nilai dari array
+// slice    : menyalin sebagian nilai dari array
+// splice   : menghapus atau mengubah element
+// sort     : mengurutkan nilai pada array
