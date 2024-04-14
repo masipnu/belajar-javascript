d2dfa24909805d1696e70fdb2445ae5fe41deb59
masipnu <masipnu1@gmail.com>
2024-04-14 16:32:29 +0700

    menambah materi : function

---
+++ function.js
@@ -0,0 +1,19 @@
+// // Fungsi lempar dadu
+// function lemparDadu() {
+//   dadu = Math.floor(Math.random() * 6) + 1;
+//   console.log(dadu);
+// }
+
+// // Fungsi dasar
+// function nyanyi() {
+//   console.log("Do re mi...!");
+// }
+// nyanyi(); //memanggil fungsi nyanyi()
+
+// Parameter dan Argumen
+//di sini disebut parameter
+function sapa(nama) {
+  console.log(`Hai ${nama}`);
+}
+//di sini disebut argument
+sapa("joko");
